ff7eda0e44e72bd537622365820e68978ef9dcd6
rodisite <66669407+rodisite@users.noreply.github.com>
2022-03-23 12:48:35 +0300

    HomeWork-04

--- mapnote/src/app/app.module.ts
+++ mapnote/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { AttributeDirective } from './attribute.directive';
+import { FormatNumberPipe } from './format-number.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    AttributeDirective
+    AttributeDirective,
+    FormatNumberPipe
   ],
   imports: [
     BrowserModule
